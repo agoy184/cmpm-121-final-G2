46c150f72aca927b70d75d993e865ad3e938b59c
Gigz <gigzpumpking@gmail.com>
2023-11-20 00:44:19 -0800

    Adding src files

---
+++ src/main.js
@@ -0,0 +1,21 @@
+let config = {
+    type: Phaser.WEBGL,
+    width: 960,
+    height: 540,
+    pixelArt: true,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            fps: 60
+        }
+    },
+    scene: [ ]
+}
+
+let game = new Phaser.Game(config);
+    
