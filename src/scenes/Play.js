8a2ac3b3acacea1b1a3ff2b674cb775e0559e1d8
Gigz <gigzpumpking@gmail.com>
2023-12-13 01:18:57 -0800

    Moved to public folder (kind of useless), added dist folder

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,4 +1,4 @@
-import * as yaml from "../../yamljs/dist/yaml.js";
+import * as yaml from "../../public/yamljs/dist/yaml.js";
 import { h, w } from "../main.js";
 import Grid from "../prefabs/Grid.js";
 import Player from "../prefabs/Player.js";
